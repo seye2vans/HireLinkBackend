6ccbeab15ec85656320608ede395c7ff569bce3a
Victor Olaleye <vansvictor79@gmail.com>
2025-10-30 10:16:46 +0100

    Edited alot

--- hirelink/src/main/java/com/example/hirelink/HirelinkApplication.java
+++ hirelink/src/main/java/com/example/hirelink/HirelinkApplication.java
@@ -2,12 +2,14 @@ package com.example.hirelink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.hirelink")
+@EnableJpaRepositories("com.example.hirelink.Repositories")
+@EntityScan("com.example.hirelink.User")
 public class HirelinkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HirelinkApplication.class, args);
 	}
-
 }
