cfe0edd4e6debb3e3676beb65fa99d52e2716602
Victor Olaleye <vansvictor79@gmail.com>
2025-11-08 13:06:59 +0100

    edited the Job Controller file

--- hirelink/src/main/java/com/example/hirelink/Job/Job.java
+++ hirelink/src/main/java/com/example/hirelink/Job/Job.java
@@ -1,11 +1,9 @@
 package com.example.hirelink.Job;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import com.example.hirelink.Application.Application;
 import com.example.hirelink.User.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -43,10 +41,6 @@ public class Job {
     @CreationTimestamp
     private LocalDateTime postedDate;
 
-    // Optional: dynamic count or can keep as field
     private int applicants;
 
-    // Add applications list with cascade delete
-    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Application> applications;
 }
